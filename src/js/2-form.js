53f7cc87e26d428e6467d2b2f1c7d4f4ffcb154d
Dmytro Troitskiy <dmitriysabbath@gmail.com>
2024-05-21 21:23:33 +0300

    Resolve task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,11 +1,53 @@
 import '../css/2-form.css';
 
-const formData = {
-  email: '',
-  message: '',
-};
+document.addEventListener('DOMContentLoaded', function () {
+  const formData = {
+    email: '',
+    message: '',
+    clear() {
+      this.email = '';
+      this.message = '';
+    },
+  };
 
-const form = document.querySelector('.feedback-form');
-form.addEventListener('input', handleInput);
+  const form = document.querySelector('.feedback-form');
+  form.addEventListener('submit', handleSubmit);
+  form.addEventListener('input', handleInput);
 
-function handleInput(event) {}
+  const LS_KEY = 'feedback-form-state';
+  const savedFormData = getFormDataFromLocalStorage();
+
+  if (savedFormData) {
+    formData.email = form.email.value = savedFormData.email;
+    formData.message = form.message.value = savedFormData.message;
+  }
+
+  function getFormDataFromLocalStorage() {
+    const savedData = localStorage.getItem(LS_KEY);
+    if (savedData) {
+      return JSON.parse(savedData);
+    }
+    return null;
+  }
+
+  function handleInput(event) {
+    const key = event.target.name;
+    const value = event.target.value.trim();
+    formData[key] = value;
+
+    localStorage.setItem(LS_KEY, JSON.stringify(formData));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+
+    if (formData.email === '' || formData.message === '') {
+      return alert('Fill please all fields');
+    }
+    console.log(formData);
+    localStorage.removeItem(LS_KEY);
+    formData.clear();
+    form.reset();
+  }
+});
